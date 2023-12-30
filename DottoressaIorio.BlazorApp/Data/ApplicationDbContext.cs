f9e6ae7890eb79a1c80de3accabf9fa2128f2def
stefanomagistri <stefano.magistri@utmostworldwide.com>
2023-12-30 10:19:45 +0000

    Configured Identity Data Types in ApplicationDbContext using Fluent API

--- DottoressaIorio.BlazorApp/Data/ApplicationDbContext.cs
+++ DottoressaIorio.BlazorApp/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 using DottoressaIorio.BlazorApp.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,5 +9,20 @@ namespace DottoressaIorio.BlazorApp.Data
     {
         public DbSet<Patient> Patients { get; set; }
         public DbSet<Therapy> Therapies { get; set; }
+         
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityUser>(entity =>
+            {
+                entity.Property(m => m.ConcurrencyStamp).HasMaxLength(50).IsRequired();
+            });
+
+            builder.Entity<IdentityRole>(entity =>
+            {
+                entity.Property(m => m.ConcurrencyStamp).HasMaxLength(50).IsRequired();
+            });
+        }
     }
 }
