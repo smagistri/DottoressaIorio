85f352ed24eb6771bcefae85dca05782928b674b
stefanomagistri <stefano.magistri@utmostworldwide.com>
2024-01-04 00:48:00 +0000

    Added validators

--- DottoressaIorio.App/Models/Therapy.cs
+++ DottoressaIorio.App/Models/Therapy.cs
@@ -1,13 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DottoressaIorio.App.Models
 {
-    // Patient.cs
-
-    // Therapy.cs
     public class Therapy
     {
         public int TherapyId { get; set; }
         public DateTime CreatedDate { get; set; }
+
+        [Required(ErrorMessage = "Description is required.")]
         public string Description { get; set; }
+
         public int PatientId { get; set; }
         public Patient Patient { get; set; }
     }
--- DottoressaIorio.App/Models/TherapyTemplate.cs
+++ DottoressaIorio.App/Models/TherapyTemplate.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DottoressaIorio.App.Models;
 
 public class TherapyTemplate
 {
     public int Id { get; set; }
+    [Required(ErrorMessage = "Title is required.")]
     public string Title { get; set; }
+
+    [Required(ErrorMessage = "Description is required.")]
     public string Description { get; set; }
 }
