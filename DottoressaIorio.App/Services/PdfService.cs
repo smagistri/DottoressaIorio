c76827d4bee15fb6a83a35a91a1ad87d69c56ca2
stefanomagistri <stefano.magistri@utmostworldwide.com>
2024-01-07 13:58:44 +0000

    moved to a different folder the pdf header and footer

--- DottoressaIorio.App/Services/PdfService.cs
+++ DottoressaIorio.App/Services/PdfService.cs
@@ -42,12 +42,12 @@ public class PdfService
             WebSettings = { DefaultEncoding = "utf-8" },
             HeaderSettings = { FontName = "Calibri", FontSize = 12, Spacing = 15,
                 Right = $"Data terapia: {therapy.CreatedDate:dd/MM/yyyy}",
-                HtmUrl = "Services/PdfHeader.html"
+                HtmUrl = "_pdf_therapy/header.html"
             },
             FooterSettings = { FontName = "Calibri", FontSize = 8, Spacing = 10,
                 Left = $"Terapia Paziente: {therapy.CreatedDate:dd/MM/yyyy} - {therapy.Patient.Title} {therapy.Patient.FirstName} {therapy.Patient.LastName}",
                 Right = "Pagina [page] di [toPage]", 
-                HtmUrl = "Services/PdfFooter.html",  }
+                HtmUrl = "_pdf_therapy/footer.html",  }
         };
 
         var pdf = _converter.Convert(new HtmlToPdfDocument
