70fbf02b4625a35eac970ab85af7aed8e63379b5
stefanomagistri <stefano.magistri@utmostworldwide.com>
2024-01-09 08:46:37 +0000

    Updated style and install batch file

--- Setup/Installer/install.bat
+++ Setup/Installer/install.bat
@@ -3,6 +3,8 @@ set "destinationFolder=C:\Program Files\DottoressaIorio\"
 set "service=DottoressaIorio.App"
 set "app=DottoressaIorio.App.exe"
 
+nssm stop %service%
+nssm remove %service% confirm
 if not exist "%destinationFolder%" mkdir "%destinationFolder%"
 7z x %sourceFile% -o"%destinationFolder%" *.* -r -y
 
