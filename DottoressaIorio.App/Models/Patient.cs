d847b8746ea8cc67e441a185b683b61aaa9f96b7
stefanomagistri <stefano.magistri@utmostworldwide.com>
2024-02-03 12:02:22 +0000

    Validator translated and improved

--- DottoressaIorio.App/Models/Patient.cs
+++ DottoressaIorio.App/Models/Patient.cs
@@ -1,27 +1,43 @@
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace DottoressaIorio.App.Models;
-
-public class Patient : IDataHandler
+namespace DottoressaIorio.App.Models
 {
-    public int PatientId { get; set; }
-    public string? Title { get; set; }
-    [Required(ErrorMessage = "First Name is required.")]
-    public string FirstName { get; set; }
-    [Required(ErrorMessage = "Last Name is required.")]
-    public string LastName { get; set; }
-    [Required(ErrorMessage = "Date of Birth is required.")]
-    [DataType(DataType.Date)]
-    public DateTime? DateOfBirth { get; set; }
-    public string? PlaceOfBirth { get; set; }
-    public string? Gender { get; set; }
-    public string? PhoneNumber { get; set; }
-    [Required(ErrorMessage = "Email is required.")]
-    [EmailAddress(ErrorMessage = "Invalid Email Address.")]
-    public string Email { get; set; }
-    public List<Therapy> Therapies { get; set; }
-    public DateTime CreatedDate { get; set; }
-    public DateTime? EditDate { get; set; }
-    public bool Deleted { get; set; }
-}
+    public class Patient : IDataHandler
+    {
+        public int PatientId { get; set; }
 
+        public string? Title { get; set; }
+
+        [Required(ErrorMessage = "Il Nome è obbligatorio.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Il Cognome è obbligatorio.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "La Data di Nascita è obbligatoria.")]
+        [DataType(DataType.Date)]
+        [Range(typeof(DateTime), "1900-01-01", "2099-12-31", ErrorMessage = "La data di nascita deve essere compresa tra il 1900/01/01 e il 2099/12/31.")]
+        public DateTime? DateOfBirth { get; set; }
+
+        public string? PlaceOfBirth { get; set; }
+
+        public string? Gender { get; set; }
+
+        [RegularExpression(@"^\+?(\d{1,4})?[-. ]?(\(\d{1,4}\))?[ .-]?(\d{1,10})$", ErrorMessage = "Il formato del numero di telefono non è valido.")]
+        public string? PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "L'indirizzo email è obbligatorio.")]
+        [EmailAddress(ErrorMessage = "Indirizzo email non valido.")]
+        public string Email { get; set; }
+
+        public List<Therapy> Therapies { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime? EditDate { get; set; }
+
+        public bool Deleted { get; set; }
+    }
+}
