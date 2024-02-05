f70fa5b7c2b324573c335156b3b5662854741a6f
stefanomagistri <stefano.magistri@utmostworldwide.com>
2024-02-05 16:07:50 +0000

    Installer

--- DottoressaIorio.App/publish.bat
+++ DottoressaIorio.App/publish.bat
@@ -2,20 +2,22 @@
 setlocal
 
 rem Define the destination folder for publishing
-set "destinationFolder=C:\Users\stefa\repo\DottoressaIorio\Setup\Service"
+set "destinationFolder=..\Setup\Service"
 
 rem Publish the .NET Core application to the destination folder
 dotnet publish -c Release -o %destinationFolder%
 
-rem Copy utility files (libwkhtmltox.dll and nssm.exe) to the destination folder
-copy .\_utility\libwkhtmltox.dll %destinationFolder%
-copy .\_utility\nssm.exe %destinationFolder%
+rem Copy utility files to the destination folder
+copy .\_utility\*.* %destinationFolder%
 
 rem Navigate to the Installer folder
-cd ..\Setup\Installer
+cd ..\Setup
 
 rem Delete the existing Service.7z file
 del "Service.7z"
 
 rem Create a new compressed archive (zip) of the Service folder
-7z a -tzip "Service.7z" "..\Service\*"
+7z a -tzip "Service.7z" ".\Service\*"
+
+rem Remove the publish directory and its contents
+rmdir /s /q ".\Service"
