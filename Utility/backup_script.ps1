ee6f5883ec73f6ab7da2bd7f5a96032aeff8ffe6
stefanomagistri <stefano.magistri@utmostworldwide.com>
2024-02-06 11:46:43 +0000

    Missing dll to load

--- Utility/backup_script.ps1
+++ Utility/backup_script.ps1
@@ -4,6 +4,8 @@ $onedrivePath = [System.IO.Path]::Combine($env:USERPROFILE, "OneDrive")
 $vaultPath = [System.IO.Path]::Combine($onedrivePath, "Personal Vault")
 $backupPath = [System.IO.Path]::Combine($vaultPath, $serviceName)
 
+$backupPath2 = "C:\Program Files\DottoressaIorio\_db_backup\"
+
 # Check if the backup folder exists, create it if not
 if (-not (Test-Path $backupPath -PathType Container)) {
     New-Item -Path $backupPath -ItemType Directory
@@ -14,6 +16,10 @@ if (-not (Test-Path $backupPath -PathType Container)) {
 
 # Copy the item with a timestamped backup file name
 Copy-Item $sourcePath ([System.IO.Path]::Combine($backupPath, "app_backup_$(Get-Date -Format 'yyyyMMddHHmmss').db"))
+
+# Copy the item with a timestamped backup file name 2
+Copy-Item $sourcePath "$backupPath\app_backup_$(Get-Date -Format 'yyyyMMddHHmmss').db"
+
 
 # Start the installed service using NSSM
 .\nssm start $serviceName
