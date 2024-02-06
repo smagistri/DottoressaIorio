dee958d3560162a2225254a18d3ab105a21e1d10
stefanomagistri <stefano.magistri@utmostworldwide.com>
2024-02-06 11:36:00 +0000

    created vault backup

--- Utility/backup_script.ps1
+++ Utility/backup_script.ps1
@@ -1,6 +1,8 @@
 $sourcePath = "C:\Program Files\DottoressaIorio\app.db"
-$backupPath = "C:\Program Files\DottoressaIorio\_db_backup\"
 $serviceName = "DottoressaIorio.App"
+$onedrivePath = [System.IO.Path]::Combine($env:USERPROFILE, "OneDrive")
+$vaultPath = [System.IO.Path]::Combine($onedrivePath, "Personal Vault")
+$backupPath = [System.IO.Path]::Combine($vaultPath, $serviceName)
 
 # Check if the backup folder exists, create it if not
 if (-not (Test-Path $backupPath -PathType Container)) {
@@ -11,7 +13,7 @@ if (-not (Test-Path $backupPath -PathType Container)) {
 .\nssm stop $serviceName
 
 # Copy the item with a timestamped backup file name
-Copy-Item $sourcePath "$backupPath\app_backup_$(Get-Date -Format 'yyyyMMddHHmmss').db"
+Copy-Item $sourcePath ([System.IO.Path]::Combine($backupPath, "app_backup_$(Get-Date -Format 'yyyyMMddHHmmss').db"))
 
 # Start the installed service using NSSM
 .\nssm start $serviceName
