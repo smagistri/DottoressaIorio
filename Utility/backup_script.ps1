1a30ebc420190771c79b02981e145eb67ba3be89
stefanomagistri <stefano.magistri@utmostworldwide.com>
2024-02-06 11:15:41 +0000

    updated backup file

--- Utility/backup_script.ps1
+++ Utility/backup_script.ps1
@@ -1,10 +1,17 @@
 $sourcePath = "C:\Program Files\DottoressaIorio\app.db"
 $backupPath = "C:\Program Files\DottoressaIorio\_db_backup\"
+$serviceName = "DottoressaIorio.App"
 
 # Check if the backup folder exists, create it if not
 if (-not (Test-Path $backupPath -PathType Container)) {
     New-Item -Path $backupPath -ItemType Directory
 }
 
+# Stop the service using NSSM
+.\nssm stop $serviceName
+
 # Copy the item with a timestamped backup file name
 Copy-Item $sourcePath "$backupPath\app_backup_$(Get-Date -Format 'yyyyMMddHHmmss').db"
+
+# Start the installed service using NSSM
+.\nssm start $serviceName
